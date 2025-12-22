f2eb9b8e1dd47ef1ff14e4de035f4f9a5809667a
0x189D7997 <199489335+0x189D7997@users.noreply.github.com>
2025-12-22 20:54:27 +0000

    Add constructors for each use of ArtifactMetadata

--- src/lib/java/com/github/winplay02/gitcraft/manifest/metadata/ArtifactMetadata.java
+++ src/lib/java/com/github/winplay02/gitcraft/manifest/metadata/ArtifactMetadata.java
@@ -1,4 +1,18 @@
 package com.github.winplay02.gitcraft.manifest.metadata;
 
 public record ArtifactMetadata(String id, String path, String sha1, long size, long totalSize, String url) {
+	// Asset index
+	public ArtifactMetadata(String id, String sha1, long size, long totalSize, String url) {
+		this(id, null, sha1, size, totalSize, url);
+	}
+
+	// Libraries
+	public ArtifactMetadata(String path, String sha1, long size, String url) {
+		this(null, path, sha1, size, 0, url);
+	}
+
+	// Client server and mappings
+	public ArtifactMetadata(String sha1, long size, String url) {
+		this(null, null, sha1, size, 0, url);
+	}
 }
